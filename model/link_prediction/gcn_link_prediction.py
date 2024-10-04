5012e08af62288a63b1ee8f5a3b87908974d907b
Vincenzo Marco De Luca <43376130+VincenzoMarcoDeLuca@users.noreply.github.com>
2024-10-04 02:04:00 +0200

    Update gcn_link_prediction.py

--- model/link_prediction/gcn_link_prediction.py
+++ model/link_prediction/gcn_link_prediction.py
@@ -5,20 +5,24 @@ import torch.nn as nn
 from torch.nn.functional import relu
 
 class GCN_link_prediction(nn.Module):
-    def __init__(self, in_channels, hidden_channels=[256,32], dropout_probability = [0.0,0.0]):
+    def __init__(self, in_channels, hidden_channels=[256,32], dropout_probability = [0.0,0.0], use_bn = False):
         super().__init__()
-        self.batch_norm1 = BatchNorm(in_channels)
-        self.batch_norm2 = BatchNorm(hidden_channels[0])
+        self.use_bn = use_bn
+        if use_bn:
+            self.batch_norm1 = BatchNorm(in_channels)
+            self.batch_norm2 = BatchNorm(hidden_channels[0])
         self.dropout1 = Dropout(p=dropout_probability[0])
         self.conv1 = GCNConv(in_channels, hidden_channels[0])
         self.dropout2 = Dropout(p=dropout_probability[1])
         self.conv2 = GCNConv(hidden_channels[0], hidden_channels[1])
 
     def encode(self, x, edge_index):
-        #x = self.batch_norm1(x)
+        if self.use_bn:
+            x = self.batch_norm1(x)
         x = self.dropout1(x)
         x = self.conv1(x, edge_index)
-        #x = self.batch_norm2(x)
+        if self.use_bn:
+            x = self.batch_norm2(x)
         x = relu(x)
         x = self.dropout2(x)
         return self.conv2(x, edge_index)
