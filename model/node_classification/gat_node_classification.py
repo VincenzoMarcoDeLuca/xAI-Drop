118bcb4c44903dde728351b19b3f30aedec32206
Vincenzo Marco De Luca <43376130+VincenzoMarcoDeLuca@users.noreply.github.com>
2024-10-04 02:02:29 +0200

    Update gat_node_classification.py

--- model/node_classification/gat_node_classification.py
+++ model/node_classification/gat_node_classification.py
@@ -1,42 +1,44 @@
 from torch_geometric.nn import GATConv, BatchNorm
 from torch.nn import Linear, ReLU, Dropout, Softmax
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class GAT_node_classification(nn.Module):
     def __init__(self, input_features, hidden_channels=[], num_heads=[], dropout=[], num_classes=2):
         super(GAT_node_classification, self).__init__()
 
 
+        if self.use_bn:
+            self.batch_norm0 = BatchNorm(input_features)
+            self.batch_norm1 = BatchNorm(hidden_channels[0])
         self.dropout1 = Dropout(p=dropout[0])
         self.conv1 = GATConv(input_features, hidden_channels[0], heads = num_heads[0])
         self.relu = ReLU()
         self.dropout2 = Dropout(p=dropout[1])
-
-        self.conv2 = GATConv(hidden_channels[0]*num_heads[0], hidden_channels[1], heads = num_heads[1])
-        self.linear = Linear(hidden_channels[1]*num_heads[1], num_classes = num_classes)
+        self.linear = Linear(hidden_channels[0]*num_heads[0], num_classes = num_classes)
 
     def forward(self,x,edge_index):
+        if self.use_bn:
+            x = self.batch_norm0(x)
+        x = F.dropout(x, self.dropout_p[0],self.training)
         x = self.conv1(x, edge_index)
-
-        #x = self.batchnorm1(x)
-
-        x = self.dropout1(x)
-        x = self.relu(x)
-        x = self.conv2(x, edge_index)
-
-        #x = self.batchnorm2(x)
-        x = self.dropout2(x)
-        x = self.leaky_relu2(x)
+        if self.use_bn:
+            x = self.batch_norm1(x)
+        x= self.relu(x)
+        x = F.dropout(x, self.dropout_p[1],self.training)
         x = self.linear(x)
         return x
 
     def full_forward(self, x, edge_index):
+        if self.use_bn:
+            x = self.batch_norm0(x)
+        x = F.dropout(x, self.dropout_p[0],self.training)
         x = self.conv1(x, edge_index)
-        x = self.relu(x)
-        x = self.dropout1(x)
-        x = self.conv2(x, edge_index)
-        x = self.relu(x)
-        x = self.dropout2(x)
+        if self.use_bn:
+            x = self.batch_norm1(x)
+        x= self.relu(x)
+        x = F.dropout(x, self.dropout_p[1],self.training)
         return x
 
     def forward_single(self, x, edge_index):
