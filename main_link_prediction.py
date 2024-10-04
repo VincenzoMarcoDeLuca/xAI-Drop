f81b2f0ac935d8db3a8557cf5fdc3804fb89a215
Vincenzo Marco De Luca <43376130+VincenzoMarcoDeLuca@users.noreply.github.com>
2024-10-04 04:10:19 +0200

    Update main_link_prediction.py

--- main_link_prediction.py
+++ main_link_prediction.py
@@ -1,8 +1,7 @@
 from dataset.utils_edge import GraphLoader
 import torch.nn as nn
 import torch.optim as optim
-from model.link_prediction.gcn_link_prediction import GCN_link_prediction
-from model.link_prediction.utils import train_link_predictor, eval_link_predictor
+from model.link_prediction import GCN_link_prediction, GAT_link_prediction,GIN_link_prediction, train_link_predictor, eval_link_predictor
 from graphxai.explainers.grad import GradExplainer
 import torch
 import numpy as np
@@ -17,52 +16,51 @@ from config import Selector
 config_path = r"config_cora_gcn_link_pr.json"
 config = Selector(config_path).args
 
-
-paths= {"data": "./data/",
-       "results": "./results/"}
-
-
-def main_function(hidden_channels, dropout, optimizer_name, loss_name, drop_edge_probability, lr, weight_decay, num_epochs,
-              dataset_name,
-              n_subgraph = 4,
-              model_path = "",
-              threshold_confidence_value = 0.95,
-              confidence_strategy = 'threshold',
-              sparsity_value = 0.1,
-              default_drop_probability = 0.5,
-              device = 'cuda',
-              id = 0):
-    torch.manual_seed(id)
-    ds_loader = GraphLoader(seed=id, n_subgraphs=n_subgraph, paths=paths, device=device, dataset_name=dataset_name)
+def main_function(config, model_path,seed = 0):
+    paths = {"data": "./data/",
+             "results": "./results/"}
+    torch.manual_seed(seed)
+    ds_loader = GraphLoader(seed=seed, paths=paths, device=config.model.device, dataset_name=config.model.dataset_name)
     train_data = ds_loader.train_data
     val_data = ds_loader.validation_data
     tst_data = ds_loader.test_data
-    model = GCN_link_prediction(train_data.x.shape[1], hidden_channels=hidden_channels, dropout_probability = dropout).to(device)
-    if optimizer_name == 'adam':
-       optimizer = optim.Adam(params=model.parameters(), lr=lr,weight_decay=weight_decay)
+    if config.model.model_name == 'gcn':
+        model = GCN_link_prediction(train_data.x.shape[1],
+                                    hidden_channels=config.model.hidden_channels,
+                                    dropout_probability = config.model.dropout).to(config.model.device)
+
+    elif config.model.model_name == 'gat':
+        model = GAT_link_prediction(train_data.x.shape[1],
+                                    hidden_channels=config.model.hidden_channels,
+                                    dropout_probability = config.model.dropout).to(config.model.device)
+    elif config.model.model_name == 'gin':
+        model = GIN_link_prediction(train_data.x.shape[1],
+                                    hidden_channels=config.model.hidden_channels,
+                                    dropout_probability = config.model.dropout).to(config.model.device)
+
+    if config.model.optimizer_name == 'adam':
+       optimizer = optim.Adam(params=model.parameters(), lr=config.model.lr,weight_decay=config.model.weight_decay)
     else:
-       print(optimizer_name, " is not available as optimizer")
-    if loss_name == 'BCEWithLogitsLoss':
+       print(config.model.optimizer_name, " is not available as optimizer")
+    if config.model.criterion_name == 'BCEWithLogitsLoss':
        criterion = nn.BCEWithLogitsLoss()
     else:
-      print(loss_name, " is not available as loss")
+      print(config.model.criterion_name, " is not available as loss")
 
 
     best_val_auc = 0.0
     explainer = GradExplainer(model=model, decoder=model.decode, criterion=criterion)
-    for epoch in range(num_epochs):
+    for epoch in range(config.model.num_epochs):
 
        trn_auc, trn_ap, trn_acc, trn_loss = train_link_predictor(model = model,
                                                          train_data = train_data,
                                                          optimizer = optimizer,
                                                          criterion = criterion,
                                                          explainer=explainer,
-                                                         drop_edge_probability=drop_edge_probability,
-                                                         threshold_confidence_value=threshold_confidence_value,
-                                                         confidence_strategy=confidence_strategy,
-                                                         default_drop_probability = default_drop_probability,
-                                                         sparsity_value = sparsity_value,
-                                                         device = device)
+                                                         drop_edge_probability=config.model.drop_edge_probability,
+                                                         threshold_confidence_value=config.model.threshold_confidence_value,
+                                                         sparsity_value = config.model.sparsity_value,
+                                                         device = config.model.device)
        val_auc, val_ap, val_loss, val_acc = eval_link_predictor(model, val_data, criterion)
        if val_auc > best_val_auc:
               best_val_auc = val_auc
@@ -82,24 +80,11 @@ history_tst_ap = []
 history_tst_loss = []
 history_tst_acc = []
 for id in range(5):
+    seed = config.model.seed + id
     model_path = config.model.dataset_name + "_" + config.model.model_name + "_" + config.model.task +str(id)+".pt"
-    tst_auc, tst_ap, tst_loss, tst_acc = main_function(hidden_channels = config.model.hidden_channels,
-                                                           dropout = config.model.dropout,
-                                                           optimizer_name = config.model.optimizer_name,
-                                                           loss_name = config.model.criterion_name,
-                                                           drop_edge_probability = config.model.drop_edge_probability,
-                                                           lr = config.model.lr,
-                                                           weight_decay = config.model.weight_decay,
-                                                           num_epochs = config.model.num_epochs,
-                                                           sparsity_value = config.model.sparsity_value,
-                                                           id = id,
-                                                           dataset_name=config.model.dataset_name,
-                                                           threshold_confidence_value=config.model.threshold_confidence_value,
-                                                           confidence_strategy=config.model.confidence_strategy,
-                                                           default_drop_probability = config.model.drop_edge_probability,
-                                                           device = config.model.device,
-                                                           n_subgraph=config.model.n_subgraphs,
-                                                           model_path = model_path)
+    tst_auc, tst_ap, tst_loss, tst_acc = main_function(config = config, model_path = model_path, seed = seed)
+
+
     history_tst_auc.append(tst_auc)
     history_tst_ap.append(tst_ap)
     history_tst_loss.append(tst_loss)
