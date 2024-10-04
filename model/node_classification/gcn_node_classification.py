bbe41f59c1eb1533e5d513c95fdfea786e884f40
Vincenzo Marco De Luca <43376130+VincenzoMarcoDeLuca@users.noreply.github.com>
2024-10-04 02:02:15 +0200

    Update gcn_node_classification.py

--- model/node_classification/gcn_node_classification.py
+++ model/node_classification/gcn_node_classification.py
@@ -2,7 +2,6 @@ from torch_geometric.nn import GCNConv, BatchNorm
 from torch.nn import Linear, Dropout, ReLU, Softmax, ELU
 from torch_geometric.utils import dropout_edge
 import torch.nn.functional as F
-import torch
 
 
 import torch.nn as nn
@@ -16,12 +15,9 @@ class GCN_node_classification(nn.Module):
             self.batch_norm1 = BatchNorm(hidden_channels[0])
 
         self.conv1 = GCNConv(input_features, hidden_channels[0])
-        self.relu = ReLU()#torch.nn.LeakyReLU(0.1)#ReLU()#torch.nn.LeakyReLU(leaky_relu1)
+        self.relu = ReLU()
         self.dropout_p = dropout
-        self.conv2 = GCNConv(hidden_channels[0], num_classes)#hidden_channels[1])
-        #self.linear = Linear(hidden_channels[0], num_classes)
-        #self.linear = Linear(hidden_channels[1], num_classes)
-        #self.batch_norm2 = BatchNorm(hidden_channels[1])
+        self.conv2 = GCNConv(hidden_channels[0], num_classes)
         self.dropout_edge_p = dropout_edge_p
 
     def forward(self,x,edge_index):
@@ -29,9 +25,7 @@ class GCN_node_classification(nn.Module):
            x = self.batch_norm0(x)
         if self.training:
             edge_index, _ = dropout_edge(edge_index, self.dropout_edge_p)
-            #noise_std = 0.01  # standard deviation of the noise
-            # Inject noise
-            #x = x + noise_std * torch.randn_like(x)
+
         x = F.dropout(x, self.dropout_p[0],self.training)
         x = self.conv1(x, edge_index)
         if self.use_bn:
@@ -39,21 +33,17 @@ class GCN_node_classification(nn.Module):
         x= self.relu(x)
         x = F.dropout(x, self.dropout_p[1], self.training)
         x = self.conv2(x, edge_index)
-        #x = self.batch_norm2(x)
-        #x = self.relu(x)
-        #x = F.dropout(x, self.dropout_p[2], self.training)
-        #x = self.linear(x)
         return x
 
-
-
     def full_forward(self, x, edge_index):
+        if self.use_bn:
+            x = self.batch_norm0(x)
+        x = F.dropout(x, self.dropout_p[0],self.training)
         x = self.conv1(x, edge_index)
+        if self.use_bn:
+            x = self.batch_norm1(x)
         x = self.relu(x)
-        x = self.dropout1(x)
-        x = self.conv2(x, edge_index)
-        x = self.relu(x)
-        x = self.dropout2(x)
+        x = F.dropout(x, self.dropout_p[0],self.training)
         return x
 
     def forward_single(self, x, edge_index):
