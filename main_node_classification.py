5c1a3c77c9cd6f23dac2b4408444dd8fafa11603
Vincenzo Marco De Luca <43376130+VincenzoMarcoDeLuca@users.noreply.github.com>
2024-10-04 04:10:32 +0200

    Update main_node_classification.py

--- main_node_classification.py
+++ main_node_classification.py
@@ -1,8 +1,6 @@
 import torch
-from dataset.utils import Dataset
-from model.node_classification.gcn_node_classification import GCN_node_classification
-from model.node_classification.gat_node_classification import GAT_node_classification
-from model.node_classification.utils import train, test
+from dataset.utils_node import Dataset
+from model.node_classification import GCN_node_classification, GAT_node_classification, GIN_node_classification, train, test
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import numpy as np
@@ -10,74 +8,72 @@ import torch.nn as nn
 from graphxai.explainers.grad import GradExplainer
 from config import Selector
 
-
-config_path = r"config_cora_gat_node_cl.json"
+config_path = r"config_cora_gin_node_cl.json"
 config = Selector(config_path).args
 
-def main_function(num_epochs, lr, weight_decay, dropout, use_bn, hidden_channels, scheduler_name, scheduler_patience, scheduler_factor, scheduler_threshold,
-                  confidence_strategy, threshold_confidence_value, confidence_sampling,
-                  threshold_sparsity_value, sparsity_value, sparsity_strategy, seed, data, dropping_distribution = 'gaussian', optimizer_name='adam', criterion_name='ce', task = 'node_cl',
-                  save_model_path = "", dropping_node_probability = 0.5, model_name='gcn', dropout_edge_probability=0.0, num_heads = 1,device='cuda'):
-    vary_dropping_probability = False
-    torch.manual_seed(seed)
+def main_function(config, save_model_path = ""):
+    torch.manual_seed(config.model.seed)
     best_val_acc = 0.0
 
-    if model_name == 'gcn':
+    if config.model.model_name == 'gcn':
         model = GCN_node_classification(input_features = num_features,
-                        hidden_channels=hidden_channels,
-                        dropout=dropout,
+                        hidden_channels=config.model.hidden_channels,
+                        dropout=config.model.dropout,
                         num_classes=num_classes,
-                        dropout_edge_p=dropout_edge_probability,
-                        use_bn = use_bn).to(device)
-    elif model_name == 'gat':
+                        dropout_edge_p=config.model.dropout_edge_probability,
+                        use_bn = config.model.use_bn).to(config.model.device)
+    elif config.model.model_name == 'gat':
         model = GAT_node_classification(input_features = num_features,
-                        hidden_channels=hidden_channels,
-                        num_heads = num_heads,
-                        dropout=dropout,
+                        hidden_channels=config.model.hidden_channels,
+                        num_heads = config.model.num_heads,
+                        dropout=config.model.dropout,
                         num_classes=num_classes,
-                        dropout_edge_p=dropout_edge_probability,
-                        use_bn = use_bn).to(device)
+                        dropout_edge_p=config.model.dropout_edge_probability,
+                        use_bn = config.model.use_bn).to(config.model.device)
+    elif config.model.model_name == 'gin':
+        model = GIN_node_classification(input_features = num_features,
+                        hidden_channels=config.model.hidden_channels,
+                        dropout=config.model.dropout,
+                        num_classes=num_classes,
+                        dropout_edge_p=config.model.dropout_edge_probability,
+                        use_bn = config.model.use_bn).to(config.model.device)
     else:
+        print("Model ", config.model.model_name, " is not available")
         exit(1)
-    if criterion_name == 'ce':
+    if config.model.criterion_name == 'ce':
         criterion = nn.CrossEntropyLoss()
     else:
-        print("Loss criterion: ", criterion_name, " not known")
+        print("Loss criterion: ", config.model.criterion_name, " not known")
         exit(1)
-    if optimizer_name == 'adam':
-        optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    elif optimizer_name == 'adamw':
-        optimizer = optim.AdamW(model.parameters(), lr = lr, weight_decay=weight_decay)
+    if config.model.optimizer_name == 'adam':
+        optimizer = optim.Adam(model.parameters(), lr=config.model.lr, weight_decay=config.model.weight_decay)
+    elif config.model.optimizer_name == 'adamw':
+        optimizer = optim.AdamW(model.parameters(), lr = config.model.lr, weight_decay=config.model.weight_decay)
     else:
-        print("Optimizer: ", optimizer_name, " not available")
+        print("Optimizer: ", config.model.optimizer_name, " not available")
         exit(1)
-    if scheduler_name == 'ReduceLROnPlateau':
+    if config.model.scheduler_name == 'ReduceLROnPlateau':
         scheduler = ReduceLROnPlateau(optimizer, mode='min',
-                                      factor=scheduler_factor,
-                                      patience=scheduler_patience,
-                                      threshold=scheduler_threshold,
-                                  threshold_mode='abs')
+                                      factor=config.model.scheduler_factor,
+                                      patience=config.model.scheduler_patience,
+                                      threshold=config.model.scheduler_threshold,
+                                      threshold_mode='abs')
     else:
-        print("Scheduler: ", scheduler_name, " is not known")
+        print("Scheduler: ", config.model.scheduler_name, " is not known")
         exit(1)
 
 
     no_improvement_val_acc = 0
 
     explainer = GradExplainer(model, criterion)
-    for epoch in range(num_epochs):
+    for epoch in range(config.model.num_epochs):
 
       trn_res = train(model, data, data.train_mask, criterion, optimizer, num_classes,
-                      confidence_sampling = confidence_sampling,
-                      dropping_probability=dropping_node_probability,
+                      dropping_probability=config.model.dropping_node_probability,
                       explainer = explainer,
-                      threshold_confidence_value=threshold_confidence_value,
-                      confidence_strategy=confidence_strategy,
-                      sparsity_value = sparsity_value,
-                      sparsity_strategy=sparsity_strategy,
-                      sparsity_threshold=threshold_sparsity_value,
-                      vary_probability=vary_dropping_probability,
-                      dropping_distribution=dropping_distribution)
+                      threshold_confidence_value=config.model.threshold_confidence_value,
+                      sparsity_value = config.model.sparsity_value,
+                      device = config.model.device)
       val_res = test(model, data, data.val_mask, criterion, num_classes)
       scheduler.step(val_res[0])
 
@@ -89,6 +85,7 @@ def main_function(num_epochs, lr, weight_decay, dropout, use_bn, hidden_channels
       else:
           no_improvement_val_acc = no_improvement_val_acc+1
 
+
       print(f"Epoch {epoch}  Trn Accuracy: {trn_res[1]:.3f}, Val Accuracy: {val_res[1]:.3f} "
                 f" Trn Loss: {trn_res[0]:.3f}, Val Loss: {val_res[0]:.3f}")
     model.load_state_dict(torch.load(save_model_path))
@@ -96,6 +93,7 @@ def main_function(num_epochs, lr, weight_decay, dropout, use_bn, hidden_channels
     best_tst_acc = best_tst_res[1]
 
     return best_tst_acc
+
 dataset = Dataset(dataset_name = config.model.dataset_name, device = config.model.device).dataset
 num_features = dataset[0].x.shape[1]
 num_classes = torch.unique(dataset[0].y).shape[0]
@@ -110,37 +108,10 @@ data.test_mask = data.test_mask.to(config.model.device)
 best_tst_acc = []
 for id in range(5):
     seed = id
-    current_tst_acc = main_function(num_epochs = config.model.num_epochs,
-              lr = config.model.lr,
-              weight_decay = config.model.weight_decay,
-              dropout = config.model.dropout,
-              data = data,
-              use_bn = config.model.use_bn,
-              hidden_channels = config.model.hidden_channels,
-              scheduler_name = config.model.scheduler_name,
-              scheduler_patience = config.model.scheduler_patience,
-              scheduler_factor = config.model.scheduler_factor,
-              scheduler_threshold = config.model.scheduler_threshold,
-              confidence_strategy = config.model.confidence_strategy,
-              threshold_confidence_value = config.model.threshold_confidence_value,
-              confidence_sampling = config.model.confidence_sampling,
-              threshold_sparsity_value = config.model.threshold_sparsity_value,
-              sparsity_value = config.model.sparsity_value,
-              sparsity_strategy = config.model.sparsity_strategy,
-              seed = config.model.seed+id,
-              optimizer_name = config.model.optimizer_name,
-              criterion_name=config.model.criterion_name,
-              task = config.model.task,
-              device = config.model.device,
-              dropout_edge_probability = config.model.dropout_edge_probability,
-              dropping_node_probability = config.model.dropping_node_probability,
-              model_name=config.model.model_name,
-              dropping_distribution = config.model.dropping_distribution,
-              num_heads = config.model.num_heads,
-              save_model_path=config.model.dataset_name + "_" + config.model.model_name + "_" + config.model.task + "_model_"+str(id)+".pt",
-              )
+    save_model_path = config.model.dataset_name + "_" + config.model.model_name + "_" + config.model.task + "_model_"+str(id)+".pt"
+    current_tst_acc = main_function(config,save_model_path=save_model_path)
     print("Experiment ID: ",id, " Test accuracy: ", current_tst_acc)
     best_tst_acc.append(current_tst_acc)
 
 best_tst_acc = np.array(best_tst_acc)
-print("Mean accuracy: ", np.mean(best_tst_acc), " ", np.std(best_tst_acc))
+print("Mean accuracy: ", np.mean(best_tst_acc), " +- ", np.std(best_tst_acc))
